fd4e2381ec6de8cc900d7c8ecdfcbd82d4644fd3
Martin van Zijl <martin.vanzijl@gmail.com>
2022-01-12 08:53:20 +1300

    Update linux release scripts to work on other PCs. [Issue #6]

--- deploy-for-linux.sh
+++ deploy-for-linux.sh
@@ -4,8 +4,36 @@
 # Script to make the Linux deployment.
 # ----------------------------------------------------------------------
 
+# Get the current directory.
+CURRENT_DIR=$PWD
+
+# Create the "AppDir" folder.
+APP_DIR=~/AppDirs/PictureGrid.AppDir
+
+#APP_DIR="$CURRENT_DIR/AppDirs/PictureGrid.AppDir"
+mkdir -p $APP_DIR
+
+# Copy the icon.
+ICON_DIR="$APP_DIR/PictureGrid/share/icons/hicolor/256x256/apps"
+mkdir -p $ICON_DIR
+
+cp images/app-icon.png $ICON_DIR/PictureGrid.png
+
+# Copy the desktop file.
+DESKTOP_DIR="$APP_DIR/PictureGrid/share/applications"
+mkdir -p $DESKTOP_DIR
+
+cp linux-deploy-files/PictureGrid.desktop $DESKTOP_DIR 
+
 # Copy the executable.
-cp ~/Documents/build-PictureGrid-Desktop-Release/PictureGrid ~/AppDirs/PictureGrid.AppDir/PictureGrid/bin/
+EXECUTABLE_DIR="$APP_DIR/PictureGrid/bin"
+mkdir -p $EXECUTABLE_DIR
+
+cp ../build-PictureGrid-Desktop-Release/PictureGrid $EXECUTABLE_DIR
+
+#cp ~/Documents/build-PictureGrid-Desktop-Release/PictureGrid ~/AppDirs/PictureGrid.AppDir/PictureGrid/bin/
 
 # Make the deployment.
-~/Downloads/build-linuxdeployqt-Desktop-Debug/bin/linuxdeployqt ~/AppDirs/PictureGrid.AppDir/PictureGrid/share/applications/PictureGrid.desktop -verbose=2 -appimage -ignore-glob="/home/martin/*"
+~/Downloads/build-linuxdeployqt-Desktop-Debug/bin/linuxdeployqt "$DESKTOP_DIR/PictureGrid.desktop" -verbose=2 -appimage -ignore-glob="/home/martin/*"
+
+#~/Downloads/build-linuxdeployqt-Desktop-Debug/bin/linuxdeployqt ~/AppDirs/PictureGrid.AppDir/PictureGrid/share/applications/PictureGrid.desktop -verbose=2 -appimage -ignore-glob="/home/martin/*"
---
+++ doc/linux-deployment.md
@@ -0,0 +1,28 @@
+# Deployment
+
+## Linux
+
+To create the App Image file for Linux, do the following:
+
+1. Install the libraries required for "LinuxDeployQt". On Ubuntu:
+
+	sudo apt install qtwebengine5-dev
+
+1. Install the "patchelf" program. On Ubuntu:
+
+	sudo apt install patchelf
+
+1. Download the App Image for the "appimagetool" software. You can get this from GitHub. Put the image in an executable directory, and make the file executable:
+
+	chmod u+x appimagetool
+
+1. Download the code for "LinuxDeployQt" [using this link](https://github.com/probonopd/linuxdeployqt/releases/tag/continuous). For example:
+
+	wget https://codeload.github.com/probonopd/linuxdeployqt/tar.gz/refs/tags/continuous
+
+1. Open the "LinuxDeployQt" project in Qt Creator.
+1. Build the debug version.
+1. Put the executable you built in "~/Downloads/build-linuxdeployqt-Desktop-Debug/bin/".
+1. Go to the PictureGrid code folder and run "deploy-for-linux.sh".
+
+You should see the App Image created in the same folder.
